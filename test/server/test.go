586166f2546ca5db4029941df1eb015a3fb89807
tikiatua <tikiatua@gmail.com>
2016-08-10 13:59:07 +0200

    update test code

--- test/server/test.go
+++ test/server/test.go
@@ -2,12 +2,17 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
+	"time"
+
 	"github.com/jinzhu/configor"
 )
 
 type Configuration struct {
 	Message string
 }
+
+var letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
 
 func main() {
 
@@ -18,4 +23,24 @@ func main() {
 	}
 	fmt.Printf("It works: %s\n", config.Message)
 	fmt.Println("And now also with automatic updates..")
+
+	taskid := uuid(10)
+
+	for {
+		fmt.Printf("%s: still running\n", taskid)
+		time.Sleep(1 * time.Second)
+	}
+
+}
+
+func uuid(strlen int) string {
+
+	rand.Seed(time.Now().UTC().UnixNano())
+	const chars = "abcdefghijklmnopqrstuvwxyz0123456789"
+	result := make([]byte, strlen)
+	for i := 0; i < strlen; i++ {
+		result[i] = chars[rand.Intn(len(chars))]
+	}
+
+	return string(result)
 }
