5cbe795ef04af00c29a7e94a762000329dd411d5
tikiatua <tikiatua@gmail.com>
2016-08-15 14:38:10 +0200

    create a data only container image

---
+++ data/noop.go
@@ -0,0 +1,8 @@
+// We must pass a command to start a container with compose up. This simple
+// noop utility will do exactly nothing when started and can be used as the start
+// command. This solution was inspired by the tianon/true docker image
+package main
+
+func main() {
+	// Do nothing
+}
