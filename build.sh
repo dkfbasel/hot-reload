05865b51c74cd200abedb34cce137cbaf3d340f5
Ramon Saccilotto <tikiatua@gmail.com>
2025-12-14 13:29:54 +0100

    Update to go version 1.25.5

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 # !/bin/sh
 
 # build the hot reload utility and a respective docker container
-docker build -t dkfbasel/hot-reload-go:1.25.3 -f ./Dockerfile ./cmd/hot-reload
+docker build -t dkfbasel/hot-reload-go:1.25.5 -f ./Dockerfile ./cmd/hot-reload
